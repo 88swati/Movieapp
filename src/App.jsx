de5e453a83a8b2aa537469ff641c3fa39f06e3e6
swati <swati442001@gmail.com>
2023-07-17 21:42:37 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -19,20 +19,19 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import Header from './Components/Header';
-import Cards from "./Components/Cards";
-import AddMovie from "./Components/AddMovie";
 
+import AddMovie from "./Components/AddMovie";
+import  Home   from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+    
       <Router>
        <Routes>
-        
-        <Route path="/" element={ <Cards/> } />
+        <Route path="/" element={ <Home/> } />
         <Route path="/addmovie" element={ <AddMovie/> } />
+        
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Cards from '../Components/Cards';
+import Layout from '../Components/Layout';
+const Home = () => {
+  return (
+    <Layout>
+        
+         <Cards/>
+    </Layout>
+  )
+}
+
+export default Home;
